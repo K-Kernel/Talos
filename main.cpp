91674b2634a0bc1e3c356db0b9f5ced976cec264
KodeVemba <kiamidiogo1@gmail.com>
2026-07-07 01:11:51 +0100

    Add addition and siLU activaiton

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include "iostream"
+#include <cmath>
+#include <iostream>
 #include <vector>
 
 struct Tensor {
@@ -7,10 +8,13 @@ struct Tensor {
 
   int row() const { return shape[0]; };
   int column() const { return shape[1]; };
+
+  // one is for read purposes only and the other is for write
+  float at(int r, int c) const { return data[r * column() + c]; }
   float &at(int r, int c) { return data[r * column() + c]; }
 };
 
-Tensor matmul(Tensor &a, Tensor &b) {
+Tensor matmul(const Tensor &a, const Tensor &b) {
   Tensor result{std::vector<float>(a.row() * b.column(), 0),
                 {a.shape[0], b.shape[1]}};
   for (int i{0}; i < a.row(); ++i) {
@@ -24,17 +28,29 @@ Tensor matmul(Tensor &a, Tensor &b) {
   return result;
 }
 
-int main() {
-  Tensor matrix_a{{1, 2, 3, 4, 5, 6}, {2, 3}};
-  Tensor matrix_b{{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, {3, 4}};
-  auto resultTensor = matmul(matrix_a, matrix_b);
+Tensor matadd(const Tensor &a, const Tensor &b) {
+  Tensor result{std::vector<float>(a.data.size()), a.shape};
+  for (int i{0}; i < a.data.size(); ++i) {
+    result.data[i] = a.data[i] + b.data[i];
+  }
+  return result;
+}
 
-  for (int i{0}; i < resultTensor.row(); ++i) {
-    for (int j{0}; j < resultTensor.column(); ++j) {
-      std::cout << resultTensor.at(i, j) << " ";
+Tensor SiLU(const Tensor &a) {
+  Tensor result = a;
+  for (int i{0}; i < result.data.size(); ++i) {
+    result.data[i] = result.data[i] / (1 + std::exp(-result.data[i]));
+  }
+  return result;
+};
+
+void outputTensor(const Tensor &a) {
+  for (int i{0}; i < a.row(); ++i) {
+    for (int j{0}; j < a.column(); ++j) {
+      std::cout << a.at(i, j) << " ";
     }
     std::cout << '\n';
   }
-
-  return 0;
 }
+
+int main() { return 0; }
